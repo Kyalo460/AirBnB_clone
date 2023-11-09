b29baa51ca6e3b34f035d0becff948b18d456441
edoka <peter.edoka.242464@unn.edu.ng>
2023-11-09 10:18:54 +0100

    feat: basemodel to and from dictionary

---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Base Model module.
+
+This module contains the BaseModel class
+that defines all common attributes/methods for other classes.
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """BaseModel Definition.
+
+    This defines all common attributes/methods for other classes.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Construct Method."""
+        if len(kwargs) == 0:
+            # new instance
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+        else:
+            # instance with a dictionary representation
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    # converts created_at and updated_at from str to datetime.
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    # skip the __class__ attribute.
+                    setattr(self, key, value)
+
+    def save(self):
+        """Save method updates the public instance attribute updated_at."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """to_dict returns the dictionary rep of the instance."""
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict["created_at"] = str(self.created_at.isoformat())
+        new_dict["updated_at"] = str(self.updated_at.isoformat())
+        return new_dict
+
+    def __str__(self):
+        """__str__ method returns the string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
